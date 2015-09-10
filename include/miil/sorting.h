1adc0aa7ed1f9c2e28e684aca6cfa79f3db33fed
David Freese <dfreese@stanford.edu>
2015-09-10 14:42:06 -0700

    added support for comparison with parameters

--- include/miil/sorting.h
+++ include/miil/sorting.h
@@ -64,6 +64,46 @@ int insertion_sort(
     return(0);
 }
 
+ /*! \brief Perform insertion sort on an stl container using a custom comparison
+  *
+  * Sorts the values within a container using a custom function within the
+  * insertion sort algorithm.  This is nearly linear for mostly sorted data.
+  * The custom function is useful for sorting classes or structures where one
+  * element of the class should be used for sorting.  This template is for
+  * custom functions requiring two arguments
+  *
+  * \param array The type of array that will be sorted.  Must have a size_type,
+  *              as well as a value_type, which all stl containers will.
+  * \param f The custom comparison function that should be used. This is a func
+  *          with four arguments.  It evaluates to true if the first argument
+  *          should be place first, or false if the second argument should be.
+  *          The third through fifth arguments are for parameters within the
+  *          comparison.
+  *
+  * \return 0 on success, less than zero on error.  No error checking is
+  *         currently implemented.
+  */
+template <class T, class Arg1, class Arg2, class Arg3>
+int insertion_sort(
+        T & array,
+        bool (*f) (typename T::value_type,
+                   typename T::value_type,
+                   Arg1, Arg2, Arg3),
+        Arg1 arg1, Arg2 arg2, Arg3 arg3)
+{
+    for (typename T::size_type ii = 1; ii < array.size(); ii++) {
+        for (typename T::size_type kk = ii;
+             (0 < kk) && f(array[kk], array[kk-1], arg1, arg2, arg3);
+             kk--)
+        {
+            typename T::value_type temp = array[kk];
+            array[kk] = array[kk - 1];
+            array[kk - 1] = temp;
+        }
+    }
+    return(0);
+}
+
 /*! \brief Sort stl container using the insertion sort method with a saved key
  *
  * Sorts the values within a container using the '<' operator within the
