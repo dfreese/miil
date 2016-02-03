e5b63ca59c55bc7a97d1637491bd699a93d73269
David Freese <dfreese@stanford.edu>
2016-02-03 00:25:11 -0800

    added script to set env variable

---
+++ set_libmiil.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Got this from stack overflow
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [ -n "${LIBMIIL_DIR}" ]; then
+  echo LIBMIIL_DIR=${LIBMIIL_DIR}
+  if [ -d ${LIBMIIL_DIR} ]; then
+      # ANDIR PREVIOUSLY DEFINED, REMOVE FROM $LD_LIBRARY_PATH
+      export LD_LIBRARY_PATH=`echo $LD_LIBRARY_PATH | sed -e 's#:'"${LIBMIIL_DIR}"'/lib##'`
+  fi
+fi
+
+LIBMIIL_DIR=$DIR
+
+export LIBMIIL_DIR
+
+if [[ $LD_LIBRARY_PATH != ?(*:)${LIBMIIL_DIR}/lib?(:*) ]]; then
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$LIBMIIL_DIR/lib
+fi
+
