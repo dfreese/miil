3e3b00122f765db56ae7f81367a990922a3400b3
David Freese <dfreese@stanford.edu>
2016-08-04 11:39:53 -0700

    remove pymiil to separate repository

--- set_libmiil.sh
+++ set_libmiil.sh
@@ -5,22 +5,18 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 if [ -n "${LIBMIIL_DIR}" ]; then
     if [ -d ${LIBMIIL_DIR} ]; then
-        # PREVIOUSLY DEFINED, REMOVE FROM $LD_LIBRARY_PATH
-        LD_LIBRARY_PATH=`echo $LD_LIBRARY_PATH | sed -e 's#:'"${LIBMIIL_DIR}"'/lib##'`
+        # remove the previously defined LIBMIIL_DIR and it's leading ':'
+        LD_LIBRARY_PATH=`echo $LD_LIBRARY_PATH | sed -e 's#:'"${LIBMIIL_DIR}"'/lib##g'`
+        # remove the previously defined LIBMIIL_DIR without a leading ':'
+        # couldn't get a \? escape on the : to work for some reason.
+        LD_LIBRARY_PATH=`echo $LD_LIBRARY_PATH | sed -e 's#'"${LIBMIIL_DIR}"'/lib##g'`
     fi
 fi
 
-if [ -n "${LIBMIIL_DIR}" ]; then
-    if [ -d ${LIBMIIL_DIR} ]; then
-        # PREVIOUSLY DEFINED, REMOVE FROM $LD_LIBRARY_PATH
-        PYTHONPATH=`echo $PYTHONPATH| sed -e 's#:'"${LIBMIIL_DIR}"'/python##'`
-    fi
-fi
 
 LIBMIIL_DIR=$DIR
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$LIBMIIL_DIR/lib
-PYTHONPATH=$PYTHONPATH:$LIBMIIL_DIR/python
 
 export LIBMIIL_DIR
 export LD_LIBRARY_PATH
-export PYTHONPATH
+
